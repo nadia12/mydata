14e5873a7e240eb63085133d1ce496491baf4f25
Poppy Sari <poppy@volantis.io>
2019-03-11 15:38:27 +0700

    add folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import configStore from '../redux/store'
 import MyData from './my-data'
+import ErrorBoundary from '../components/error-boundary'
 
 const store = configStore()
 
 export default () => (
-  <Provider store={store}>
-    <MyData />
-  </Provider>
+  <ErrorBoundary>
+    <Provider store={store}>
+      <MyData />
+    </Provider>
+  </ErrorBoundary>
 )
