e6cc0e0784d8c52b74622e985448432e36c11983
elishaenu <elisha@volantis.io>
2019-03-26 13:46:11 +0700

    remove unused style code

--- src/pages/my-data/create/units/file/units/step1/index.js
+++ src/pages/my-data/create/units/file/units/step1/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import StepOne from './units'
+import StepOneFile from './units'
 
 const mapStateToProps = state => ({
 })
--- src/pages/my-data/create/units/file/units/step1/units/index.js
+++ src/pages/my-data/create/units/file/units/step1/units/index.js
@@ -1,4 +1,4 @@
-import { H3Styled, ColumnMultilineStyled, ColumnStyled } from './style'
+import { H3Styled, ColumnStyled } from './style'
 import React from 'react'
 import { Label } from 'volantis-ui'
 import {
@@ -12,19 +12,17 @@ const StepOneFile = (props) => {
   return (
     <>
       <H3Styled>Choose File</H3Styled>
-      <ColumnMultilineStyled>
-        <ColumnStyled>
-          Please select on how you want to upload your file. You can browse your local storage or insert the URL to upload your file. You can upload CSV or XLS/XLSX file in My Data.
-        </ColumnStyled>
-        <ColumnStyled>
-          <Label>How do you upload your file?</Label>
-          <RadioGroup handleChangeInput={handleChangeInput} value={fields.uploadType} name="uploadType" radioLists={uploadTypeList} />
-        </ColumnStyled>
-        <ColumnStyled>
-          <Label>Which file type do you want to upload?</Label>
-          <RadioGroup handleChangeInput={handleChangeInput} value={fields.fileType} name="fileType" radioLists={fileTypeList} />
-        </ColumnStyled>
-      </ColumnMultilineStyled>
+      <ColumnStyled>
+        Please select on how you want to upload your file. You can browse your local storage or insert the URL to upload your file. You can upload CSV or XLS/XLSX file in My Data.
+      </ColumnStyled>
+      <ColumnStyled>
+        <Label>How do you upload your file?</Label>
+        <RadioGroup handleChangeInput={handleChangeInput} value={fields.uploadType} name="uploadType" radioLists={uploadTypeList} />
+      </ColumnStyled>
+      <ColumnStyled>
+        <Label>Which file type do you want to upload?</Label>
+        <RadioGroup handleChangeInput={handleChangeInput} value={fields.fileType} name="fileType" radioLists={fileTypeList} />
+      </ColumnStyled>
     </>
   )
 }
