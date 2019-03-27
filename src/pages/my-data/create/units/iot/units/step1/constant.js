742ecda373ae174d1cddf06b867cb5e9bbe70bcc
elishaenu <elisha@volantis.io>
2019-03-27 13:38:38 +0700

    STEP 1 / IoT

---
+++ src/pages/my-data/create/units/iot/units/step1/constant.js
@@ -0,0 +1,5 @@
+export const radioLists = [
+  { label: 'Sensor', value: 'sensor', disabled: false },
+  { label: 'Broker', value: 'broker', disabled: true },
+  { label: 'Gateway', value: 'gateway', disabled: true }
+];
---
+++ src/pages/my-data/create/units/iot/units/step1/units/index.js
@@ -0,0 +1,30 @@
+import { H3Styled, ColumnStyled } from './style'
+import React from 'react'
+import { Label } from 'volantis-ui'
+import {
+  radioLists
+} from '../constant'
+import RadioGroup from '../../../../radio-group'
+
+const StepOneIot = (props) => {
+  const { handleChangeInput, fields } = props
+  return (
+    <>
+      <H3Styled>Device Type</H3Styled>
+      <ColumnStyled>
+        Please select your IoT Device type that you want to connect to the system.
+      </ColumnStyled>
+      <ColumnStyled>
+        <Label>Which device type do you want to use?</Label>
+        <RadioGroup handleChangeInput={() => null} value={fields.deviceType || 'sensor'} name="deviceType" radioLists={radioLists} />
+      </ColumnStyled>
+    </>
+  )
+}
+
+StepOneIot.propTypes = {
+  handleChangeInput: PropTypes.func.isRequired,
+  fields: PropTypes.object.isRequired
+}
+
+export default StepOneIot
