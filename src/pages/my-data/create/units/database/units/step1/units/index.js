c73ba0647b1d59bf1f7eade9282872aa2e3566ee
elishaenu <elisha@volantis.io>
2019-03-28 18:23:07 +0700

    apply ui kit to step 1 database

--- src/pages/my-data/create/units/database/units/step1/units/index.js
+++ src/pages/my-data/create/units/database/units/step1/units/index.js
@@ -1,6 +1,7 @@
-import { H3Styled, ColumnStyled } from './style'
+import { Cols } from '../../../../style'
+import { ColsStyled } from './style'
 import React from 'react'
-import { Select } from 'volantis-ui'
+import { Select, Subtitle, Body } from 'volantis-ui'
 import {
   typeOptions
 } from '../constant'
@@ -9,11 +10,17 @@ const StepOneDatabase = (props) => {
   const { handleChangeInput, fields } = props
   return (
     <>
-      <H3Styled>Database Type</H3Styled>
-      <ColumnStyled>
+      <Cols padding={16}>
+        <Subtitle size="big" type="primary">
+          Database Type
+        </Subtitle>
+      </Cols>
+      <Cols padding={24}>
+        <Body type="secondary">
         Please select Database type you want to connect. You can choose between MySQL, PostgreSQL, MS SQL, DB2 or Oracle.
-      </ColumnStyled>
-      <ColumnStyled>
+        </Body>
+      </Cols>
+      <ColsStyled padding={0}>
         <Select
           label="WHICH DATABASE DO YOU WANT TO USE?"
           name="WHICH DATABASE DO YOU WANT TO USE?"
@@ -21,7 +28,7 @@ const StepOneDatabase = (props) => {
           options={typeOptions}
           isOptionDisabled={(option) => option.value === ''}
           onChange={(selected) => handleChangeInput({ key: 'dbType', value: selected.value })} value={{ value: fields.dbType, label: fields.dbType }} />
-      </ColumnStyled>
+      </ColsStyled>
     </>
   )
 }
--- src/pages/my-data/create/units/database/units/step1/units/style.js
+++ src/pages/my-data/create/units/database/units/step1/units/style.js
@@ -1,15 +1,12 @@
 import styled from 'styled-components'
+import { Cols } from '../../../../style'
 import colors from '../../../../../../../../../src/assets/css/colors'
 
 export const H3Styled = styled.h3`
   color: ${colors.gold}
 `
 
-export const ColumnStyled = styled.div`
-  width: 100%;
-  padding: 0.75rem;
-  padding-left: 0;
-
+export const ColsStyled = styled(Cols)`
   > div {
     width: 240px;
   }
