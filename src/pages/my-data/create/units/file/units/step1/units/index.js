b891648206567c6decc017522660b29e5a6b237b
elishaenu <elisha@volantis.io>
2019-03-28 18:01:43 +0700

    apply ui kit for step1 file

--- src/pages/my-data/create/units/file/units/step1/units/index.js
+++ src/pages/my-data/create/units/file/units/step1/units/index.js
@@ -1,6 +1,6 @@
-import { H3Styled, ColumnStyled } from './style'
+import { Cols } from '../../../../style'
 import React from 'react'
-import { Label } from 'volantis-ui'
+import { Label, Subtitle, Body } from 'volantis-ui'
 import {
   uploadTypeList,
   fileTypeList
@@ -11,18 +11,24 @@ const StepOneFile = (props) => {
   const { handleChangeInput, fields } = props
   return (
     <>
-      <H3Styled>Choose File</H3Styled>
-      <ColumnStyled>
+      <Cols padding={16}>
+        <Subtitle size="big" type="primary">
+          Choose File
+        </Subtitle>
+      </Cols>
+      <Cols padding={24}>
+        <Body type="secondary">
         Please select on how you want to upload your file. You can browse your local storage or insert the URL to upload your file. You can upload CSV or XLS/XLSX file in My Data.
-      </ColumnStyled>
-      <ColumnStyled>
-        <Label>How do you upload your file?</Label>
+        </Body>
+      </Cols>
+      <Cols padding={24}>
+        <Label>HOW DO YOU UPLOAD YOUR FILE?</Label>
         <RadioGroup handleChangeInput={handleChangeInput} value={fields.uploadType} name="uploadType" radioLists={uploadTypeList} />
-      </ColumnStyled>
-      <ColumnStyled>
-        <Label>Which file type do you want to upload?</Label>
+      </Cols>
+      <Cols padding={24}>
+        <Label>WHICH FILE TYPE DO YOU WANT TO UPLOAD?</Label>
         <RadioGroup handleChangeInput={handleChangeInput} value={fields.fileType} name="fileType" radioLists={fileTypeList} />
-      </ColumnStyled>
+      </Cols>
     </>
   )
 }
