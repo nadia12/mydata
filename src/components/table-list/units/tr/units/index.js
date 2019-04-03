d75f53f2e070f53ac1a8e03bdda1befbeb68e6bf
GK <adji@kofera.com>
2019-04-03 17:47:16 +0700

    install eslint and minor code changes

--- src/components/table-list/units/tr/units/index.js
+++ src/components/table-list/units/tr/units/index.js
@@ -1,51 +1,58 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Tr = ({en, isSelected, oneClick, doubleClick, rightClick, iconSvg, ICON}) => {
-  const setIcon = iconSvg || ICON;
+const Tr = ({
+  en,
+  isSelected,
+  oneClick,
+  doubleClick,
+  rightClick,
+  iconSvg,
+  ICON
+}) => {
+  const setIcon = iconSvg || ICON
+
   return (
     <tr
-      key={`tr-file-${en.idx}`} 
-      onContextMenu={!!rightClick.isActive ? (evt) => rightClick.action(evt, en) : null} 
-      onClick={!!oneClick.isActive ? (evt) => oneClick.action(evt, en) : null} 
-      onDoubleClick={!!doubleClick.isActive ? (evt) => doubleClick.action(evt, en) : null}
-      className={ !!isSelected ? 'is-active' : '' } 
+      key={`tr-file-${en.idx}`}
+      onContextMenu={rightClick.isActive ? evt => rightClick.action(evt, en) : () => {}}
+      onClick={oneClick.isActive ? evt => oneClick.action(evt, en) : () => {}}
+      onDoubleClick={doubleClick.isActive ? evt => doubleClick.action(evt, en) : () => {}}
+      className={!!isSelected && 'is-active'}
     >
-      {/* { 
-        props.tds.map((td, idx) => {
-          return <td key={`td-${idx}`} style={{ width: td.width }}><div className={`table-icon ${isSelected ? 'icon-selected' : '' }`}>{getIcon}{td.content}</div></td>
-        })
-      } */}
-      <td style={{ width: '25.84%' }}><div className={`table-icon ${isSelected ? 'icon-selected' : '' }`}>{setIcon}{en.name}</div></td>
-      <td style={{ width: '15.94%' }}><div> {en.creatorName} </div></td>
+      <td style={{ width: '25.84%' }}>
+        <div className={`table-icon ${isSelected ? 'icon-selected' : ''}`}>
+          {setIcon}
+          {en.name}
+        </div>
+      </td>
+      <td style={{ width: '15.94%' }}><div>{en.creatorName}</div></td>
       <td style={{ width: '15.94%' }}>{ en.type }</td>
       <td style={{ width: '7.9%' }}>{en.size}</td>
       <td style={{ width: '15.94%' }}>{en.updatedAt}</td>
       <td style={{ width: '18.34%' }}>{en.status || '-'}</td>
-    </tr >
-  );
-};
+    </tr>
+  )
+}
 
 Tr.defaultProps = {
-  en: {}, 
-  isSelected: false, 
-  oneClick: {isActive: false, action: () => null},
-  doubleClick: {isActive: false, action: () => null},
-  rightClick: {isActive: false, action: () => null},
+  en: {},
+  isSelected: false,
+  oneClick: { isActive: false, action: () => {} },
+  doubleClick: { isActive: false, action: () => {} },
+  rightClick: { isActive: false, action: () => {} },
   ICON: null,
-  iconSvg: null,
+  iconSvg: null
 }
 
 Tr.propTypes = {
-  _mydataList: PropTypes.object.isRequired,
-  tds: PropTypes.array,
-  en: PropTypes.object, 
-  isSelected: PropTypes.bool, 
+  en: PropTypes.object,
+  isSelected: PropTypes.bool,
   oneClick: PropTypes.bool,
   doubleClick: PropTypes.bool,
   rightClick: PropTypes.bool,
   ICON: PropTypes.object,
-  iconSvg: PropTypes.element,
+  iconSvg: PropTypes.element
 }
 
-export default Tr;
+export default Tr
--- src/pages/my-data/list/index.js
+++ src/pages/my-data/list/index.js
@@ -1,72 +1,70 @@
-import React from 'react'
 import { connect } from 'react-redux'
+import uuidv4 from 'uuid/v4'
+
 import List from './units'
-import uuidv4 from 'uuid/v4';
 // import { getPermisson } from 'Helper/'
 
-import { 
+import {
   setHeaders,
-  setEntityList, 
+  setEntityList,
   postConnectorData,
   handleChangeMenuRight,
   handleChangeTopMenu,
   handleChangeInput,
-  handleSort,
+  handleSort
 } from './function'
 
 import {
   setToggleModal,
   setToggleModalClose,
   setToggleModalOpen,
-  setValues,
   setValue,
-  setAuthCookie,
+  setAuthCookie
 } from './reducer'
 
 import {
-  location,
   isInSystemFolder,
-  setRootLocation,
+  setRootLocation
 } from './local-helper'
 
 const mapStateToProps = state => ({
   _mydataList: state._mydataList,
-  isInSystemFolder: isInSystemFolder
+  isInSystemFolder
 })
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   setHeaders: () => dispatch(setHeaders()),
   setRootLocation: () => setRootLocation(),
-  setAuthCookie: (props) => dispatch(setAuthCookie(props)),
-  handleSort: (name) =>{
-    console.log("name=22=>", name)
-    dispatch(handleSort(name))
-  },
-  handleToggleModal: (modalType) => dispatch(setToggleModal(modalType)),
-  handleAddNewData: () => { 
+  setAuthCookie: props => dispatch(setAuthCookie(props)),
+  handleSort: name => dispatch(handleSort(name)),
+  handleToggleModal: modalType => dispatch(setToggleModal(modalType)),
+  handleAddNewData: () => {
     dispatch(setToggleModalOpen('menubar'))
+
     return dispatch(setToggleModalClose('menubarRight'))
   },
-  handleChangeTopMenu: (menu) => {
+  handleChangeTopMenu: menu => {
     dispatch(setToggleModalClose('menubar'))
+
     return dispatch(handleChangeTopMenu(menu))
   },
   handleChangeMenuRight: (menu, value) => {
     dispatch(setToggleModalClose('menubarRight'))
+
     return dispatch(handleChangeMenuRight(menu, value))
   },
   handleNewSensorGroupAdd: async (tHeaders, sensorGroupFields) => {
     //ini belum yaa
-    const groupMappingId = uuidv4();
+    const groupMappingId = uuidv4()
 
     const reqDataSG = {
       name: sensorGroupFields.sensorGroupName,
       description: sensorGroupFields.description,
       mappingScheme: null,
       groupMappingId
-    };
+    }
 
-    const headers = { ...tHeaders, 'V-NAME': sensorGroupFields.sensorGroupName };
+    const headers = { ...tHeaders, 'V-NAME': sensorGroupFields.sensorGroupName }
     // this.props.createNewSensorGroup({ reqData: reqDataSG, headers });
     // await this.props.addToSensorGroup({
     //   reqData: {
@@ -78,28 +76,28 @@ const mapDispatchToProps = (dispatch) => ({
     // this.handleSearchTypeChange(DEFAULT_TYPE_LABEL); // return the default search to all type
     // this.fetchEntityList();
     // this.toggleShow('newSensorGroup');
-    dispatch(setToggleModalClose('newSensorGroup')) //close it
+    dispatch(setToggleModalClose('newSensorGroup'))
   },
-  handleChangeInput: (params) => dispatch(handleChangeInput(params)),
+  handleChangeInput: params => dispatch(handleChangeInput(params)),
   handleMouseLeave() {
     dispatch(setToggleModalClose('menubar'))
     document.getElementById('mouse-leave').style.display = 'none'
   },
   setEntityList: () => dispatch(setEntityList()),
-  getPermission: () => dispatch(setValue("actionPermission", "")),
-  postConnectorData: (connectorIds) => { 
-    return dispatch(postConnectorData(connectorIds, (res)=>{
-      dispatch(setToggleModalOpen("entityContent")) //show entityContent Table
-      dispatch(setValue("connectorsData", res))
-    })) 
-  },
+  getPermission: () => dispatch(setValue('actionPermission', '')),
+  postConnectorData: connectorIds => (
+    dispatch(postConnectorData(connectorIds, res => {
+      dispatch(setToggleModalOpen('entityContent'))
+      dispatch(setValue('connectorsData', res))
+    }))
+  ),
   setBreadcrumb: () => {
     // const location = window.localStorage.getItem('MYDATA.location');
     // const breadcrumb = window.localStorage.getItem('MYDATA.breadcrumb');
-    console.log("setBreadcrumb")
+    console.log('setBreadcrumb')
     // localBreadcrumb.setOnLocal()
     // dispatch(setValue("inFilteredResult", localBreadcrumb()))
-  },
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(List)
