35fe96c903ca222363c1fa23dcc05ff32d776467
Nadia Rii <nadia@kofera.com>
2019-04-09 13:16:52 +0700

    function doc

--- src/pages/my-data/list/units/modal/model-detail/units/function-doc/index.js
+++ src/pages/my-data/list/units/modal/model-detail/units/function-doc/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import FunctionDoc from './units'
+import { getTrs } from './function'
 
 const mapStateToProps = state => ({
-  functionDoc: state._mydataList.functionDoc,
 })
 
 const mapDispatchToProps = dispatch => ({
+  Trs: dispatch(getTrs()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(FunctionDoc)
