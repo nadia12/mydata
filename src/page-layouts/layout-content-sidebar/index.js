1a2218c84201d676aecf7767bbf4f01558b0d59a
Nadia Rii <nadia@kofera.com>
2019-04-12 13:56:06 +0700

    fix breadcrumb onchange

--- src/page-layouts/layout-content-sidebar/index.js
+++ src/page-layouts/layout-content-sidebar/index.js
@@ -5,11 +5,6 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  handleChangeBreadCrumb: ({ redirect = ''}) => {
-    if (!!window.location && !!window.location.href) {
-      window.location.href = redirect
-    }
-  },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LayoutContentSidebar)
