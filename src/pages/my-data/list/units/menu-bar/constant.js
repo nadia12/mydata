45a76f6a74b53dd0189b7f250bb99d4ec51e10cd
ipung.anwar1@gmail.com <ipung.anwar1@gmail.com>
2019-04-22 14:58:52 +0700

    command sub menu icon feature in UI

--- src/pages/my-data/list/units/menu-bar/constant.js
+++ src/pages/my-data/list/units/menu-bar/constant.js
@@ -6,20 +6,23 @@ import {
 
 import {
   FileIcon,
-  SensorGroupIcon,
+  // SensorGroupIcon,
   FolderIcon,
   DatabaseIcon,
-  DeviceIcon,
-  ImageIcon,
+  // DeviceIcon,
+  // ImageIcon,
 } from 'volantis-icon'
 
 export const MENU_LIST = [
   {
     permission: permission.createFolder, icon: (<FolderIcon />), name: 'Folder', value: CREATE_TYPE.folder, hasBottom: false, child: [],
   },
-  {
-    permission: permission.createIot, icon: (<SensorGroupIcon />), name: 'Sensor Group', value: CREATE_TYPE.sensorgroup, hasBottom: true, child: [],
-  },
+  // ========== > FOR THIS PHASE, THIS FEATURE ON HOLD (22-04-2019)
+  // {
+  //   permission: permission.createIot, icon: (<SensorGroupIcon />), name: 'Sensor Group', value: CREATE_TYPE.sensorgroup, hasBottom: true, child: [],
+  // },
+  // ============================================================>
+
   // { icon: (<CollectionIcon />), name: 'Collection', value: 'collection', hasBottom: true, child: [
   // { icon: (<ImageIcon />), name: 'Image collection', value: 'imagecollection ', hasBottom: false, child: [] },
   // { icon: (<SensorGroupIcon />), name: 'Sensor Group', value: 'sensorgroup', hasBottom: false, child: [] }
@@ -34,15 +37,17 @@ export const MENU_LIST = [
       {
         icon: (<FileIcon />), name: 'CSV or XLS', value: CREATE_TYPE.file, hasBottom: false, child: [],
       },
-      {
-        icon: (<ImageIcon />), name: 'Image or Video', value: CREATE_TYPE.media, hasBottom: false, child: [],
-      },
+      // FOR THIS PHASE, THIS FEATURE ON HOLD (22-04-2019)
+      // {
+      //   icon: (<ImageIcon />), name: 'Image or Video', value: CREATE_TYPE.media, hasBottom: false, child: [],
+      // },
     ],
   },
   {
     permission: permission.createDatabase, icon: (<DatabaseIcon />), name: 'SQL', value: CREATE_TYPE.sql, hasBottom: false, child: [],
   },
-  {
-    permission: permission.createIot, icon: (<DeviceIcon />), name: 'IoT device', value: CREATE_TYPE.device, hasBottom: false, child: [],
-  },
+  // FOR THIS PHASE, THIS FEATURE ON HOLD (22-04-2019)
+  // {
+  //   permission: permission.createIot, icon: (<DeviceIcon />), name: 'IoT device', value: CREATE_TYPE.device, hasBottom: false, child: [],
+  // },
 ]
