338a5572491826efbfba58f72e3462a5588c0753
Nadia Rii <nadia@kofera.com>
2019-04-24 17:27:51 +0700

    remove func

--- src/components/table-list/units/tr/index.js
+++ src/components/table-list/units/tr/index.js
@@ -7,17 +7,6 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  ellipsisText: name => {
-    const maxLength = 20
-    const indexMax = (maxLength - 1)
-    let checkedName = name
-
-    if (checkedName.length > maxLength * 2) {
-      checkedName = `${checkedName.substring(0, indexMax * 2 - 5)}...`
-    }
-
-    return checkedName
-  },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tr)
