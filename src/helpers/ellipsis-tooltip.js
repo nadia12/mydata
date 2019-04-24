db6659bfec46bd8d2fae3c3a429c92263a1e869d
Nadia Rii <nadia@kofera.com>
2019-04-24 14:47:36 +0700

    helper ellipsis

---
+++ src/helpers/ellipsis-tooltip.js
@@ -0,0 +1,80 @@
+// Ellipsis With Tooltip;
+// source: https://github.com/amirfefer/react-ellipsis-with-tooltip/blob/master/src/index.js
+
+import React from 'react'
+import { Tooltip } from 'volantis-ui'
+import PropTypes from 'prop-types'
+
+const ellipsisDefaultStyle = {
+  overflow: 'hidden',
+  overflowWrap: 'break-word',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  wordBreak: 'break-all',
+}
+
+class EllipisWithTooltip extends React.Component {
+  state = {
+    hasOverflowingChildren: false,
+    text: undefined,
+  }
+
+  updateOverflow = event => {
+    const el = event.target
+    const { hasOverflowingChildren } = this.state
+
+    if (!hasOverflowingChildren && el.scrollWidth > el.clientWidth) {
+      const expectedLength = el.clientWidth * 0.12 - 5
+      this.setState({
+        hasOverflowingChildren: true,
+        text: `${el.innerText.substring(0, expectedLength)}...`,
+      })
+    }
+  }
+
+  refCallback = element => {
+    if (element) {
+      const expectedLength = element.clientWidth * 0.12 - 5
+      if (element.innerText.length > Math.round(expectedLength)) {
+        this.setState({
+          hasOverflowingChildren: true,
+          text: `${element.innerText.substring(0, expectedLength)}...`,
+        })
+      } else {
+        this.setState({ text: element.innerText })
+      }
+    }
+  }
+
+  render() {
+    const { hasOverflowingChildren, text } = this.state
+    const {
+      children,
+      position,
+    } = this.props
+    const ellipsisStyle = { ...ellipsisDefaultStyle }
+
+    return hasOverflowingChildren ? (
+      <>
+        <Tooltip message={children} position={position}>
+          {text}
+        </Tooltip>
+      </>
+    ) : (
+      <div style={ellipsisStyle} onMouseEnter={this.updateOverflow} ref={this.refCallback}>
+        {text || children}
+      </div>
+    )
+  }
+}
+
+EllipisWithTooltip.propTypes = {
+  children: PropTypes.node.isRequired,
+  position: PropTypes.string,
+}
+
+EllipisWithTooltip.defaultProps = {
+  position: 'top',
+}
+
+export default EllipisWithTooltip
