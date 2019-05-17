77a71780a1853c37728a04eff5404baee1246b07
Nadia Rii <nadia@kofera.com>
2019-05-17 11:33:11 +0700

    empty on didmount

--- src/pages/my-data/list/units/lifecycle.js
+++ src/pages/my-data/list/units/lifecycle.js
@@ -8,6 +8,7 @@ import {
 } from 'Config/lib/local-helper'
 
 const componentDidMount = props => {
+  props.setEmptyEntities()
   props.resetState()
   props.setHeaders()
   props.handleResetSelectList()
