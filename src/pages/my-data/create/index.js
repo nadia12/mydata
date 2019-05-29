ea6b6385cdbc8fc7ccc3f9c5e8a3b15ca40c2573
ipung.anwar1@gmail.com <ipung.anwar1@gmail.com>
2019-05-29 17:21:18 +0700

    refactor

--- src/pages/my-data/create/index.js
+++ src/pages/my-data/create/index.js
@@ -113,8 +113,6 @@ const mapDispatchToProps = (dispatch, props) => ({
       if (filesData.status === 'SUCCESS') {
         // success redirect my-data
         dispatch(linkToMyDataRoot(props.linkTo))
-      } else if (filesData.status === 'FAILED') {
-        dispatch(tusUploadStart({ files, authCookie, uploadUrl: `${host}/file/` }))
       } else if (files[0] && files[0].name) {
         return dispatch(tusUploadStart({ files, authCookie, uploadUrl: `${host}/file/` }))
       }
