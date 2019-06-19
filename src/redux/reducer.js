d4f07a18568fcb13bd677e9d805136d22045f395
Nadia Rii <nadia@kofera.com>
2019-06-19 17:11:42 +0700

    add mydata preview

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,11 @@
 import mydataList from 'Pages/my-data/list/reducer'
 import mydataCreate from 'Pages/my-data/create/reducer'
+import mydataPreview from 'Pages/my-data/preview/reducer'
 
 const reducer = {
   _mydataList: mydataList,
   _mydataCreate: mydataCreate,
+  _mydataPreview: mydataPreview,
 }
 
 export default reducer
