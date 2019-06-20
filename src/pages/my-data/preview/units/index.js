085270c7bba7b1564b264cc6c37419d4c19dc92a
Nadia Rii <nadia@kofera.com>
2019-06-20 13:47:17 +0700

    src preview

--- src/pages/my-data/preview/units/index.js
+++ src/pages/my-data/preview/units/index.js
@@ -7,6 +7,7 @@ import {
 import { ArrowDropdownIcon, ArrowDropupIcon } from 'volantis-icon'
 import LayoutPreview from 'PageLayouts/layout-preview'
 import dummyData from './data-sample'
+import { NoDataBoxStyle } from './style'
 
 // component
 import method from './lifecycle'
@@ -14,6 +15,7 @@ import method from './lifecycle'
 const Preview = ({
   tableHeaders,
   previewData: { result },
+  errorPreview,
   isLoadingPreview,
   infoData,
   setIcon,
@@ -34,77 +36,84 @@ const Preview = ({
 
   return (
     <>
+
       <LayoutPreview
         title={infoData.name}
-        icon={infoData.uiEntityType ? setIcon(infoData.uiEntityType) : null}
+        icon={!!infoData.uiEntityType && setIcon(infoData.uiEntityType)}
         totalRows={result.length}
+        isShowAction={!errorPreview.response}
       >
         <div className="table-preview-ds">
-          <Table tableHeight="calc(100vh - 189px)">
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th />
-                {
-                  tableHeaders.map((dataheader, indexHeader) => (
-                    <Table.Th
-                      icon={selectedHeader === indexHeader ? isAscendingSort ? ArrowDropdownIcon : ArrowDropupIcon : null}
-                      onClick={() => onHeaderClick(indexHeader)}
-                      colWidth="200px"
-                      key={`th1-${indexHeader}`}
-                      type="-"
-                    >
-                      {dataheader}
-                    </Table.Th>
-                  ))
-                }
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Th />
-                {
-                  tableHeaders.map((dataheader, indexHeader) => (
-                    <Table.Th
-                      colWidth="200px"
-                      key={`th2-${indexHeader}`}
-                    >
-                      <Input
-                        id={dataheader}
-                        name={dataheader}
-                        type="text"
-                        placeholder="Search..."
-                        value={searchColumns.dataheader}
-                        onChange={e => handleColumnChangeInput({ key: dataheader, value: e.target.value })}
-                        onKeyPress={e => {
-                          if (e.key === 'Enter') handleColumnEnter()
-                        }}
-                      />
-                    </Table.Th>
-                  ))
-                }
-              </Table.Tr>
-            </Table.Thead>
-            {!!fields.length ? (
-              <Table.Tbody>
-                {
-                  fields.map((dt, index) => (
-                    <Table.Tr key={`tr-${index}`}>
-                      <Table.Td position="center">{index + 1}</Table.Td>
-                      {
-                        Object.values(dt).map((val, indexChild) => (
-                          <Table.Td
-                            colWidth="200px"
-                            key={`td-${indexChild}`}
-                            tooltipPosition="bottom"
-                          >
-                            {val}
-                          </Table.Td>
-                        ))
-                      }
-                    </Table.Tr>
-                  ))
-                }
-              </Table.Tbody>
-            ) : <Table.Tbody />}
-          </Table>
+          {!errorPreview.response && (
+            <Table tableHeight="calc(100vh - 189px)">
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th />
+                  {
+                    tableHeaders.map((dataheader, indexHeader) => (
+                      <Table.Th
+                        icon={selectedHeader === indexHeader ? isAscendingSort ? ArrowDropdownIcon : ArrowDropupIcon : null}
+                        onClick={() => onHeaderClick(indexHeader)}
+                        colWidth="200px"
+                        key={`th1-${indexHeader}`}
+                        type="-"
+                      >
+                        {dataheader}
+                      </Table.Th>
+                    ))
+                  }
+                </Table.Tr>
+                <Table.Tr>
+                  <Table.Th />
+                  {
+                    tableHeaders.map((dataheader, indexHeader) => (
+                      <Table.Th
+                        colWidth="200px"
+                        key={`th2-${indexHeader}`}
+                      >
+                        <Input
+                          id={dataheader}
+                          name={dataheader}
+                          type="text"
+                          placeholder="Search..."
+                          value={searchColumns.dataheader}
+                          onChange={e => handleColumnChangeInput({ key: dataheader, value: e.target.value })}
+                          onKeyPress={e => {
+                            if (e.key === 'Enter') handleColumnEnter()
+                          }}
+                        />
+                      </Table.Th>
+                    ))
+                  }
+                </Table.Tr>
+              </Table.Thead>
+              {!!fields.length ? (
+                <Table.Tbody>
+                  {
+                    fields.map((dt, index) => (
+                      <Table.Tr key={`tr-${index}`}>
+                        <Table.Td position="center">{index + 1}</Table.Td>
+                        {
+                          Object.values(dt).map((val, indexChild) => (
+                            <Table.Td
+                              colWidth="200px"
+                              key={`td-${indexChild}`}
+                              tooltipPosition="bottom"
+                            >
+                              {val}
+                            </Table.Td>
+                          ))
+                        }
+                      </Table.Tr>
+                    ))
+                  }
+                </Table.Tbody>
+              ) : <Table.Tbody />}
+            </Table>
+          )}
+          {!!errorPreview.response && (
+            <NoDataBoxStyle> Oops, something went wrong...</NoDataBoxStyle>
+          )}
         </div>
 
       </LayoutPreview>
@@ -121,6 +130,7 @@ Preview.propTypes = {
   handleColumnChangeInput: PropTypes.func,
   handleColumnEnter: PropTypes.func,
   searchColumns: PropTypes.object,
+  errorPreview: PropTypes.object,
 }
 
 Preview.defaultProps = {
@@ -131,6 +141,7 @@ Preview.defaultProps = {
   handleColumnChangeInput: () => {},
   handleColumnEnter: () => {},
   searchColumns: {},
+  errorPreview: {},
 }
 
 export default lifecycle(method)(Preview)
