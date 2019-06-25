e41bb5fbb820b6e005f2acdc1826212c36f6c9ff
Nadia Rii <nadia@kofera.com>
2019-06-25 16:45:58 +0700

    setheaders on lifecycle

--- src/pages/my-data/preview/units/tabular-preview/index.js
+++ src/pages/my-data/preview/units/tabular-preview/index.js
@@ -4,17 +4,22 @@ import {
 import TabularPreview from './units'
 import {
   postPreviewData,
+  postTableHeaders,
   setTableHeaders,
 } from './function'
 
 const mapStateToProps = ({ volantisMyData: { _mydataPreview } }) => ({
-  tableHeaders: _mydataPreview.preview.tableHeaders,
+  tableHeaders: _mydataPreview.tableHeaders.data,
+  thError: _mydataPreview.tableHeaders.errorMessage,
   previewData: _mydataPreview.preview.data,
+  previewStatus: _mydataPreview.preview.status,
+  infoData: _mydataPreview.info.data,
   isLoadingPreview: _mydataPreview.preview.isLoading,
 })
 
 const mapDispatchToProps = dispatch => ({
-  setTableHeaders: () => dispatch(setTableHeaders()),
+  postTableHeaders: params => dispatch(postTableHeaders(params)),
+  setTableHeaders: params => dispatch(setTableHeaders(params)),
   postPreviewData: id => dispatch(postPreviewData(id)),
 })
 
--- src/pages/my-data/preview/units/tabular-preview/units/lifecycle.js
+++ src/pages/my-data/preview/units/tabular-preview/units/lifecycle.js
@@ -5,11 +5,15 @@ import {
 const componentDidMount = props => {
   const id = getUrlId()
   props.postPreviewData(id)
+  if (!!props.infoData.id) props.postTableHeaders()
 }
 
 const componentDidUpdate = (props, prevProps) => {
-  if (!prevProps.previewData.result.length && props.previewData.result.length && prevProps.previewData.result !== props.previewData.result) {
+  if (!prevProps.previewData.result.length && !!props.previewData.result.length && prevProps.previewData.result !== props.previewData.result) {
     props.setTableHeaders()
+  }
+  if (!prevProps.infoData.id && !!props.infoData.id && prevProps.infoData.id !== props.infoData.id) {
+    props.postTableHeaders()
   }
 }
 
